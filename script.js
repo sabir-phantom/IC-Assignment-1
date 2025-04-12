49524186770f2b5cd3b7d58e063088740e7f2562
sabir-phantom <sabir.sbair@gmail.com>
2025-04-13 01:26:39 +0600

    uploading files

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Website loaded successfully!');
+
+// Basic click event example
+document.querySelector('h1').addEventListener('click', function() {
+    console.log('You clicked the heading!');
+});
